c10624a63c5f031154215094ff048825f5413e8a
Bryan Ross <bryan@rossipedia.com>
2013-11-19 19:09:45 -0700

    initial commit

---
+++ clamps.coffee
@@ -0,0 +1,50 @@
+glob = require("glob")
+path = require("path")
+fs = require("fs")
+uglify = require("uglify-js")
+CleanCSS = require("clean-css")
+root = process.argv[2]
+rootStat = undefined
+
+unless root
+  console.log "Usage: node index.js folder"
+  process.exit -1
+
+rootStat = fs.statSync(root)
+unless rootStat and rootStat.isDirectory()
+  console.log root + " is not a directory"
+  process.exit -1
+
+sourceExp = /\.(js|css)$/
+minnedExp = /\.min\.(js|css)$/
+getMinName = (fname) -> fname.replace sourceExp, ".min.$1"
+isNotMinnedFile = (fname) -> not minnedExp.test(fname)
+relPath = (fname) -> fname.replace root.replace(/\\/g, "/"), ""
+getExt = (fname) -> sourceExp.exec(fname)[1]
+fullPath = (fname) -> path.join root, fname
+
+cleaner = new CleanCSS()
+minifiers =
+  js: (fname) -> uglify.minify(fname).code
+  css: (fname) -> cleaner.minify fs.readFileSync fname, encoding: "utf-8"
+
+needsMin = (fname) ->
+  min = getMinName(fname)
+  try
+    fs.statSync(fname).mtime > fs.statSync(min).mtime
+  catch e
+    true # statSync threw on min file name
+
+writeMinned = (fname) ->
+  minifier = minifiers[getExt(fname)]
+  minFile = getMinName(fname)
+  fs.writeFile minFile, minifier(fname), encoding: "utf-8" , (err) ->
+    throw err  if err
+    console.log "Wrote " + relPath(minFile)
+
+glob "**/*.{js,css}", cwd: root , (err, files) ->
+  throw err  if err
+  files = files.map(fullPath)
+  files = files.filter(isNotMinnedFile)
+  files = files.filter(needsMin)
+  files.forEach writeMinned
---
+++ clamps.js
@@ -0,0 +1,58 @@
+var glob = require('glob'),
+    path = require('path'),
+    fs = require('fs'),
+    uglify = require('uglify-js'),
+    CleanCSS = require('clean-css'),
+    root = process.argv[2],
+
+if (!root) {
+  console.log('Usage: node index.js folder');
+  process.exit(-1);
+}
+
+var rootStat = fs.statSync(root);
+
+if (!(rootStat && rootStat.isDirectory())) {
+  console.log(root + " is not a directory");
+  process.exit(-1);
+}
+
+var sourceExp = /\.(js|css)$/;
+var minnedExp = /\.min\.(js|css)$/;
+var getMinName = function(fname) { return fname.replace(sourceExp, '.min.$1'); };
+var isNotMinnedFile = function (fname) { return !minnedExp.test(fname); };
+var relPath = function(fname) { return fname.replace(root.replace(/\\/g, '/'), ''); };
+var getExt = function(fname) { return sourceExp.exec(fname)[1]; };
+var fullPath = function(fname) { return path.join(root, fname); }
+
+var cleaner = new CleanCSS();
+var minifiers = {
+  'js'   : function(fname) { return uglify.minify(fname).code; },
+  'css'  : function(fname) { return cleaner.minify(fs.readFileSync(fname, {encoding: 'utf-8'})); }
+};
+
+var needsMin = function(fname) {
+  var min = getMinName(fname);
+  try {
+    return fs.statSync(fname).mtime > fs.statSync(min).mtime;
+  } catch (e) {
+    return true; // statSync threw on min file name
+  }
+};
+
+var writeMinned = function(fname) {
+  var minifier = minifiers[getExt(fname)];
+  var minFile = getMinName(fname);
+  fs.writeFile(minFile, minifier(fname), {encoding:'utf-8'}, function(err) {
+    if (err) throw err;
+    console.log('Wrote ' + relPath(minFile));
+  });
+};
+
+glob("**/*.{js,css}", {cwd: root}, function(err, files) {
+  if (err) throw err;
+  files = files.map(fullPath);
+  files = files.filter(isNotMinnedFile);
+  files = files.filter(needsMin);
+  files.forEach(writeMinned);
+});
