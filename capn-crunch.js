54ab70af39e75baa9e82cabc532d7736062773f3
Bryan Ross <bryan@rossipedia.com>
2013-11-21 13:14:25 -0700

    write one to throw away

--- capn-crunch.js
+++ capn-crunch.js
@@ -5,6 +5,7 @@ var glob = require('glob'),
     CleanCSS = require('clean-css'),
     less = require('less'),
     EventEmitter = require('events').EventEmitter,
+    util = require('util'),
     root = process.argv[2];
 
 if (!root) {
@@ -12,142 +13,129 @@ if (!root) {
   process.exit(-1);
 }
 
+var events = new EventEmitter();
+
 var rootStat = fs.statSync(root);
 if (!(rootStat && rootStat.isDirectory())) {
   console.log(root + " is not a directory");
   process.exit(-1);
 }
 
-var compileFromExts = ['less'];
-var compileToExts = ['css'];
-var minifyExts = ['js', 'css'];
+var filesGlob = "**/*.{less,css,js}";
+var exclude = /\.min\.(css|js)$/;
 
-var minifyOred = minifyExts.join('|')
-var minSourceExp = new RegExp('\\.(' + minifyOred + ')$');
-var minnedExp = new RegExp('\\.min\\.(' + minifyOred + ')$');
-var minifyGlob = "**/*.{" + minifyExts.join(',') + "}";
-var getMinName = function(fname) { return fname.replace(minSourceExp, '.min.$1'); };
-var isNotMinnedFile = function (fname) { return !minnedExp.test(fname); };
-var relPath = function(fname) { return fname.replace(root.replace(/\\/g, '/'), ''); };
-var getExt = function(fname) { return /\.([^\.]+)$/.exec(fname)[1]; };
-var fullPath = function(fname) { return path.join(root, fname); }
-
-var events = new EventEmitter();
-
-var compilers = {
-  'less' : function(fname, cb) {
-    fs.readFile(fname, {encoding: 'utf-8'}, function(e, source) {
-      if (e) throw e;
-      less.render(source, {paths:[path.dirname(fname)]}, function(e,css) { if (e) throw e;
-        cb(css);
-      });
-    });
-  }
-};
+function processLess(file, source, done) {
+  var dirname = path.dirname(file);
+  var options = {paths:[dirname], compress:true};
+  less.render(source, options, done);
+}
 
 var cleaner = new CleanCSS();
-var minifiers = {
-  'js'   : function(fname) { return uglify.minify(fname).code; },
-  'css'  : function(fname) { return cleaner.minify(fs.readFileSync(fname, {encoding: 'utf-8'})); }
-};
+function processCss(file, source, done) {
+  var minCss = cleaner.minify(source);
+  done(null, minCss);
+}
 
-var needsAction = function(source, out) {
-  try { return fs.statSync(source).mtime > fs.statSync(out).mtime; }
-  catch (e) { return true; }
-};
+var compressor = uglify.Compressor({warnings:false});
+function processJs(file, source, done) {
+  var ast = uglify.parse(source);
+  ast.figure_out_scope();
+  ast = ast.transform(compressor);
+  ast.figure_out_scope();
+  ast.compute_char_frequency();
+  ast.mangle_names();
+  var code = ast.print_to_string();
+  done(null, code);
+}
 
-var needsMin = function(fname) {
-  return needsAction(fname, getMinName(fname));
-};
-
-var minify = function(fname) {
-  try {
-    fs.writeFile(getMinName(fname), minifiers[getExt(fname)](fname), {encoding:'utf-8'}, function(err) {
-      if (err) throw err;
-      events.emit('minified', fname);
-    });
-  } catch (e) {
-    console.log("Error minifying " + fname + ": " + e);
+var transformers = {
+  'less' : {
+    name: function (name) { return name.replace(/\.less$/, '.min.css'); },
+    code: processLess
+  },
+  'css'  : {
+    name : function (name) { return name.replace(/\.css$/, '.min.css'); },
+    code : processCss,
+  },
+  'js'   : {
+    name : function (name) { return name.replace(/\.js/, '.min.js'); },
+    code : processJs
   }
 };
 
-var getCompiledName = function(fname) {
-  var ext = getExt(fname);
-  var compiledName = fname.replace(new RegExp('\\.' + ext + '$'), '.' + compileToExts[compileFromExts.indexOf(ext)]);
-  return compiledName;
-};
+function getExtension(filename) {
+  return filename.match(/\.([^\.]+)$/)[1];
+}
 
-var needsCompile = function(fname) {
-  return needsAction(fname, getCompiledName(fname));
-};
+function getOutputName(filename) {
+  return transformers[getExtension(filename)].name(filename);
+}
 
-var compile = function(fname) {
-  var compiler = compilers[getExt(fname)];
-  try {
-  compiler(fname, function(output) {
-    if(output.trim().length === 0) {
-      events.emit('compiled', fname);
-      return;
-    }
-    fs.writeFile(getCompiledName(fname), output, {encoding:'utf-8'}, function(e) {
-      events.emit('compiled', fname);
-    });
-  });
-  } catch (e) {
-    console.log("Error compiling " + fname + ": " + e);
-  }
-};
-
-var compileGlob = "**/*." + (compileFromExts.length < 2 ? compileFromExts[0] : ('{' + compileFromExts.join(',') + '}'));
-var compileAll = function() {
-  glob(compileGlob, {cwd: root}, function(err, files) {
-    if (err) throw err;
-    files = files.map(fullPath);
-    files = files.filter(needsCompile);
-    if (files.length === 0) {
-      events.emit('compile-done');
-      return;
-    }
-
-    var compiled = 0;
-    var done = function(fname) {
-      if (++compiled === files.length) {
-        events.emit('compile-done');
-      }
-    };
-
-    events.on('compiled', done);
-    files.forEach(compile);
+function readFile(filename) {
+  fs.readFile(filename, {encoding:'utf-8'}, function(e, contents) {
+    if (e) throw e;
+    events.emit('file-read', filename, contents);
   });
 }
 
-var minifyAll = function() {
-  glob(minifyGlob, {cwd: root}, function(err, files) {
-    if (err) throw err;
-    files = files.map(fullPath);
-    files = files.filter(isNotMinnedFile); // Js no likey negative look-behind
-    files = files.filter(needsMin);
-
-    if(files.length === 0) {
-      events.emit('minify-done');
-      return;
-    }
-
-    var minified = 0;
-    var done = function(fname) {
-      if(++minified === files.length) {
-        events.emit('minify-done');
-      }
-    };
-    events.on('minified', done);
-    files.forEach(function(fname) { minify(fname, done); });
+function processFile(filename, contents) {
+  var transformer = transformers[getExtension(filename)].code;
+  if (!transformer) throw new Error('Unknown file format: ' + filename);
+  transformer(filename, contents, function(e, output) {
+    if (e) throw e;
+    events.emit('file-processed', filename, output);
   });
 };
 
+function writeOutputFile(filename, output) {
+  var outfilename = getOutputName(filename);
+  fs.writeFile(outfilename, output, {encoding:'utf-8'}, function(e) {
+    if (e) throw e;
+    events.emit('file-saved', filename, outfilename);
+  });
+};
+
+function findFiles() {
+  glob(filesGlob, {cwd:root}, function(e, files) {
+    if (e) throw e;
+    // Exclude
+    files = files.filter(function(f) { return !exclude.test(f); });
+    // Convert to abs path
+    files = files.map(function(f) { return path.join(root, f); });
+    events.emit('files-found', files);
+
+    var pump = function() {
+      if(files.length > 0) {
+        var file = files.shift();
+        events.emit('file-found', file);
+      } else {
+        events.removeListener('file-saved', pump);
+      }
+    };
+
+    events.on('file-saved', pump);
+
+    pump();
+  });
+};
+
+
+var processedCount = 0;
+var totalToProcess = 0;
+
 events.on('begin', function() { console.time('crunch'); })
-      .on('begin', compileAll)
-      .on('compiled', function(fname) { console.log('compiled: ' + relPath(getCompiledName(fname))); })
-      .on('compile-done', minifyAll)
-      .on('minified', function(fname) { console.log('minified: ' + relPath(getMinName(fname))); })
-      .on('minify-done', function() { console.timeEnd('crunch'); })
+      .on('begin', findFiles)
+      .on('files-found', function(files) {
+        totalToProcess = files.length;
+        console.log('found ' + totalToProcess + ' files');
+      })
+      .on('file-found', readFile)
+      .on('file-read', processFile)
+      .on('file-processed', writeOutputFile)
+      .on('file-saved', function(oldname, newname) {
+        console.log('compiled [' + getExtension(oldname) + ']: ' + newname);
+        if(++processedCount === totalToProcess)
+          events.emit('done');
+      })
+      .on('done', function() { console.timeEnd('crunch'); })
       .emit('begin');
