7493e952d2bb4a609c7b7c82b8ab7cbd3cc30dd1
Bryan Ross <bryan@rossipedia.com>
2013-12-31 15:21:03 -0700

    cache break url()s, and better error reporting

--- capn-crunch.js
+++ capn-crunch.js
@@ -5,35 +5,94 @@ var glob = require('glob'),
     CleanCSS = require('clean-css'),
     less = require('less'),
     EventEmitter = require('events').EventEmitter,
+    url = require('url'),
     util = require('util'),
-    root = process.argv[2];
+    crypto = require('crypto'),
+    staticRoot = process.argv[2],
+    siteRoot = process.argv[3]; // for resolving css url()'s that start with /
 
-if (!root) {
+if (!staticRoot) {
   console.log('Usage: node capn-crunch folder');
   process.exit(-1);
 }
 
+if (!siteRoot) {
+  siteRoot = root;
+}
+
 var events = new EventEmitter();
 
-var rootStat = fs.statSync(root);
+var rootStat = fs.statSync(staticRoot);
 if (!(rootStat && rootStat.isDirectory())) {
   console.log(root + " is not a directory");
   process.exit(-1);
 }
 
 var filesGlob = "**/*.{less,css,js}";
-var exclude = /\.min\.(css|js)$/;
+var exclude = /(\.min\.(css|js)$)|(^_(.+?)\.less$)/;
 
 function processLess(file, source, done) {
   var dirname = path.dirname(file);
   var options = {paths:[dirname], compress:true};
-  less.render(source, options, done);
+  less.render(source, options, function(e, css) {
+    if (e) fileError(file, e);
+    css = cacheBreakCss(file, css);
+    done(null, css);
+  });
 }
 
 var cleaner = new CleanCSS();
 function processCss(file, source, done) {
   var minCss = cleaner.minify(source);
+  minCss = cacheBreakCss(file, minCss);
   done(null, minCss);
+}
+
+function getHash(file) {
+  var sha = crypto.createHash('sha1');
+  sha.setEncoding('hex');
+  var contents = fs.readFileSync(file);
+  sha.write(contents);
+  sha.end();
+  return sha.read();
+}
+
+var cacheBreakers = {};
+function getCacheBreaker(file) {
+  if(typeof cacheBreakers[file] === 'undefined') {
+    cacheBreakers[file] = getHash(file);
+  }
+
+  return cacheBreakers[file];
+}
+
+function isSiteLocalImagePath(file) {
+  switch(file.charAt(0)) {
+    case '.': return true;
+    case '/': return file.charAt(1) != '/';
+    default: return false;
+  }
+}
+
+function cacheBreakCss(file, css) {
+  return css.replace(/url\((['"]?)(.+?)\1\)/g, function(match, quote, p1) {
+    // anything else isn't a file ref on the web site
+    if(!isSiteLocalImagePath(p1)) {
+      return match;
+    }
+    // Local or absolute
+    var startFrom = file[0] == '/' ? siteRoot : path.dirname(file);
+    var urlpath = path.normalize(path.join(startFrom, p1));
+    var hash;
+    try {
+      hash = getCacheBreaker(urlpath);
+    } catch (e) {
+      fileError(file, 'error cache-breaking rule ' + match + '\n' + e.toString());
+    }
+    var broken = 'url(' + quote + p1 + '?v=' + hash.substr(0,12) + quote + ')';
+    // console.log(' -> broke ' + broken);
+    return broken;
+  });
 }
 
 var compressor = uglify.Compressor({warnings:false});
@@ -50,8 +109,8 @@ function processJs(file, source, done) {
 
 var transformers = {
   'less' : {
-    name: function (name) { return name.replace(/\.less$/, '.min.css'); },
-    code: processLess
+    name : function (name) { return name.replace(/\.less$/, '.min.css'); },
+    code : processLess
   },
   'css'  : {
     name : function (name) { return name.replace(/\.css$/, '.min.css'); },
@@ -64,6 +123,13 @@ var transformers = {
     code : processJs
   }
 };
+
+function fileError(filename, e) {
+  if (e.constructor !== Error) {
+    throw new Error('error [' + filename + ']: ' + e);
+  }
+  throw e;
+}
 
 function getExtension(filename) {
   return filename.match(/\.([^\.]+)$/)[1];
@@ -80,7 +146,7 @@ function shouldRead(filename) {
 
 function readFile(filename) {
   fs.readFile(filename, {encoding:'utf-8'}, function(e, contents) {
-    if (e) throw e;
+    if (e) fileError(filename,e);
     events.emit('file-read', filename, contents);
   });
 }
@@ -88,31 +154,27 @@ function readFile(filename) {
 function processFile(filename, contents) {
   var transformer = transformers[getExtension(filename)].code;
   if (!transformer) throw new Error('Unknown file format: ' + filename);
-  try {
   transformer(filename, contents, function(e, output) {
-    if (e) throw e;
+    if (e) fileError(filename,e);
     events.emit('file-processed', filename, output);
   });
-  } catch (e) {
-    events.emit('file-error', filename, e);
-  }
 };
 
 function writeOutputFile(filename, output) {
   var outfilename = getOutputName(filename);
   fs.writeFile(outfilename, output, {encoding:'utf-8'}, function(e) {
-    if (e) throw e;
+    if (e) fileError(filename,e);
     events.emit('file-saved', filename, outfilename);
   });
 };
 
-function findFiles() {
+function findFiles(root) {
   glob(filesGlob, {cwd:root}, function(e, files) {
-    if (e) throw e;
+    if (e) fileError(filename,e);
     // Convert to abs path
     files = files.map(function(f) { return path.join(root, f); });
     // Global Exclude
-    files = files.filter(function(f) { return !exclude.test(f); });
+    files = files.filter(function(f) { return !exclude.test(path.basename(f)); });
     // Conditial check
     files = files.filter(shouldRead);
 
@@ -144,12 +206,10 @@ function findFiles() {
         events.emit('file-found', file);
       } else {
         events.removeListener('file-saved', pump);
-        events.removeListener('file-error', pump);
       }
     };
 
     events.on('file-saved', pump)
-          .on('file-error', pump);
     pump();
   });
 };
@@ -172,8 +232,14 @@ events.on('begin', function() { console.time('crunch'); })
         if(++processedCount === totalToProcess)
           events.emit('done');
       })
-      .on('file-error', function(filename, error) {
-        console.log('error [' + getExtension(filename) + '] ' + filename + ' : ' + error);
-      })
       .on('done', function() { console.timeEnd('crunch'); })
-      .emit('begin');
+      .emit('begin', staticRoot);
+
+process.on('uncaughtException', function(e) {
+  if(e.stack) {
+    console.log(e.stack);
+  } else {
+    console.log(e);
+  }
+  process.exit(-1);
+});
